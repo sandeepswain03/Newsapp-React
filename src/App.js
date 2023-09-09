d637e900f7f64ef5991122d619aa8ffb7fe69bf9
SandeepSwain <sandeepswain2004@gmail.com>
2023-09-09 14:07:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import Navbar from "./Components/Navbar";
+import News from "./Components/News";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  pageSize=9;
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <News
+                  key="general"//you have to place a unique key for routing
+                  pageSize={this.pageSize}
+                  country="in"
+                  category="general"
+                />
+              }
+            ></Route>
+            <Route
+              path="/business"
+              element={
+                <News
+                  key="business"
+                  pageSize={this.pageSize}
+                  country="in"
+                  category="business"
+                />
+              }
+            ></Route>
+            <Route
+              path="/entertainment"
+              element={
+                <News
+                  key="entertainment"
+                  pageSize={this.pageSize}
+                  country="in"
+                  category="entertainment"
+                />
+              }
+            ></Route>
+            <Route
+              path="/health"
+              element={
+                <News
+                  key="health"
+                  pageSize={this.pageSize}
+                  country="in"
+                  category="health"
+                />
+              }
+            ></Route>
+            <Route
+              path="/science"
+              element={
+                <News
+                  key="science"
+                  pageSize={this.pageSize}
+                  country="in"
+                  category="science"
+                />
+              }
+            ></Route>
+            <Route
+              path="/sports"
+              element={
+                <News
+                  key="sports"
+                  pageSize={this.pageSize}
+                  country="in"
+                  category="sports"
+                />
+              }
+            ></Route>
+            <Route
+              path="/technology"
+              element={
+                <News
+                  key="technology"
+                  pageSize={this.pageSize}
+                  country="in"
+                  category="technology"
+                />
+              }
+            ></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/Components/News.js
@@ -0,0 +1,127 @@
+import React, { Component } from "react";
+import Newsitem from "./Newsitem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
+
+export class News extends Component {
+  static defaultProps = {
+    country: "in",
+    pageSize: 6,
+    category: "general",
+  };
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  };
+
+  capitalize = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+    };
+    document.title = `${this.capitalize(this.props.category)} - QuickReads`;
+  }
+
+  async updatenews() {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ec85f0a97da049b6b19e085c65be4243&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false,
+    });
+  }
+
+  async componentDidMount() {
+    this.updatenews();
+  }
+
+  handlepeviousclick = async () => {
+    this.setState({
+      page: this.state.page - 1,
+    });
+    this.updatenews();
+  };
+
+  handlenextclick = async () => {
+    if (
+      !(
+        this.state.page + 1 >
+        Math.ceil(this.state.totalResults / this.props.pageSize)
+      )
+    ) {
+      this.setState({
+        page: this.state.page + 1,
+      });
+      this.updatenews();
+    }
+  };
+
+  render() {
+    return (
+      <div className="container my-3">
+        <h1 className="text-center my-3">
+          QuickReads-Top {this.capitalize(this.props.category)} Headlines
+        </h1>
+        {this.state.loading && <Spinner />}
+        <div className="row">
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  {/*key should be unique for each element*/}
+                  <Newsitem
+                    title={element.title ? element.title.slice(0, 45) : ""}
+                    discription={
+                      element.description
+                        ? element.description.slice(0, 88)
+                        : ""
+                    }
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                    newsdate={element.publishedAt}
+                    author={element.author}
+                    source={element.source.name}
+                  />
+                </div>
+              );
+            })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlepeviousclick}
+            disabled={this.state.page <= 1}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlenextclick}
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
+          >
+            Next &rarr;
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default News;
